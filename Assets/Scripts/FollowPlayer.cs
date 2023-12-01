e5ae973a02e0032eaad9ea190b21f468183859a0
justin-chhay <jchhay@uwaterloo.ca>
2023-12-01 16:04:18 -0500

    to move project to github

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    // "Vector3" is another data type, that stores 3 floats (is useful when recording x, y, z positions)
+    public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    // The 'f' at the end of the values signify that the value is of the "float" data type
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // We marked this as a "Fixed" update, because we are using it to mess w/ physics. Plus, Unity prefers this
+    void FixedUpdate ()
+    {
+        // Add a force on the z-axis
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);   
+
+        if ( Input.GetKey("d"))
+        {
+            //Only executed if condition is met
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            //Only executed if condition is met
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
